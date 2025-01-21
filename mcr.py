f2e39ac4d7350e7eaab1c32702e57058c5288eb4
陶源 <102897099+zzuLebron@users.noreply.github.com>
2025-01-21 17:13:20 +0800

    Add files via upload

---
+++ mcr.py
@@ -0,0 +1,55 @@
+def is_win(game):
+    win = False
+    # Check rows
+    if game[0][0] == game[0][1] == game[0][2] and (game[0][0] == 'X' or game[0][0] == 'O'):
+        win = True
+    if game[1][0] == game[1][1] == game[1][2] and (game[1][0] == 'X' or game[1][0] == 'O'):
+        win = True
+    if game[2][0] == game[2][1] == game[2][2] and (game[2][0] == 'X' or game[2][0] == 'O'):
+        win = True
+    # Check columns
+    if game[0][0] == game[1][0] == game[2][0] and (game[0][0] == 'X' or game[0][0] == 'O'):
+        win = True
+    if game[0][1] == game[1][1] == game[2][1] and (game[0][1] == 'X' or game[0][1] == 'O'):
+        win = True
+    if game[0][2] == game[1][2] == game[2][2] and (game[0][2] == 'X' or game[0][2] == 'O'):
+        win = True
+    # Check diagonals
+    if game[0][0] == game[1][1] == game[2][2] and (game[0][0] == 'X' or game[0][0] == 'O'):
+        win = True
+    if game[0][2] == game[1][1] == game[2][0] and (game[0][2] == 'X' or game[0][2] == 'O'):
+        win = True
+    return win
+
+def main():
+    game = [[' ' for _ in range(3)] for _ in range(3)]  # Tic-tac-toe board
+    player1 = 'X'
+    player2 = 'O'
+    turn = False  # False for player 1's turn, True for player 2's turn. Player 1 first.
+    print("X = Player 1")
+    print("O = Player 2")
+    for n in range(9):
+        turn = not turn  # Switch turns
+        if not turn:
+            print("Player 1: ", end="")
+        else:
+            print("Player 2: ", end="")
+        print("Which cell to mark? i:[1..3], j:[1..3]: ")
+        i, j = map(int, input().split())
+        i -= 1
+        j -= 1
+        if not turn:
+            game[i][j] = 'X'
+        else:
+            game[i][j] = 'O'
+        if is_win(game):
+            print("Win!")
+            break  # Terminate the game
+        if n == 8:  # All cells have been filled
+            print("Tie!")
+        # Show the game board
+        for row in game:
+            print(" ".join(row))
+
+if __name__ == "__main__":
+    main()
